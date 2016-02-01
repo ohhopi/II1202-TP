0a90924fcb66479475bd618ff96c126a3d21b7e4
ohhopi <tristan.muratore@gmail.com>
2016-02-01 10:04:45 +0100

    TP 1

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
 
-
 public class Pave {
-    public static void main(String ... args) {
-
-	//1. Demander chaque longueur grâce au Scanner
-	Scanner scan = new Scanner(System.in);
-	
-    }
+	public static void main(String [] args) {
+		// Nouveau Scanner
+		Scanner scan = new Scanner(System.in);
+		//Déclaration des variables (et leur type)
+		double a,b,c;
+		//Série d'inputs des côtes du parralélipipède
+		System.out.println("Hauteur du pave : ");
+		a = forcePositiveDouble(scan);
+		System.out.println("Largeur du pave : ");
+		b = forcePositiveDouble(scan);
+		System.out.println("Longueur du pave : ");
+		c = forcePositiveDouble(scan);
+		//Calcul du Volume
+		double volume = a*b*c;
+		//Afficher le volume
+		System.out.println("Volume total : " + volume);
+	}
+	public static double forcePositiveDouble(Scanner scan) {
+		double d = -1;
+		while(d < 0) {
+			System.out.print("Veuillez entrer une valeur positive s'il vous plait : ");
+			d = scan.nextDouble();
+		}
+		return d;
+	}
 }
+
+// nicolas.rousset@isep.fr
---
+++ src/SecondDegre.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SecondDegre {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		double a,b,c;
+		String sortie;
+		System.out.println("Veuillez saisir la valeur de a (aX^2+bX+c) : ");
+		a = scan.nextDouble();
+		System.out.println("Veuillez saisir la valeur de b (aX^2+bX+c) : ");
+		b = scan.nextDouble();
+		System.out.println("Veuillez saisir la valeur de c (aX^2+bX+c) : ");
+		c = scan.nextDouble();
+		double [] solution = solve(a, b, c);
+		if (solution != null){
+			System.out.println("Soltion(s) : "+ solution[0] + " | " + solution[1]);
+		}else{
+			System.out.println("Pas de solution reele.");
+		}
+	}
+
+	public static double [] solve(double a, double b, double c) {
+		double discriminant = (b*b) - (4*a*c);
+		double [] sortie;
+		if( a != 0.0 && discriminant > 0) {
+			// 2 Racines Simples
+			sortie = new double[2];
+			double racineB = Math.sqrt(discriminant);
+			sortie[0] = (-b - racineB) / (2*a);
+			sortie[1] = (-b + racineB) / (2*a);
+		} else if( a !=0.0 && discriminant == 0.0) {
+			// Racine Double
+			sortie = new double[1];
+			sortie[0] = -b / (2*a);
+		}
+		else {
+			// 3e Cas (Racines complexes, non traite)
+			sortie = null;
+		}
+		return sortie;
+	}
+}
