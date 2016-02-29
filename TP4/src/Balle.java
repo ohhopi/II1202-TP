a8615877f5743cbe86e26cb5ffaf6b4004987c21
ohhopi <tristan.muratore@gmail.com>
2016-02-29 12:25:01 +0100

    Ajout de la détection de collision

--- TP4/src/Balle.java
+++ TP4/src/Balle.java
@@ -1,51 +1,41 @@
 
 public class Balle {
 	
-	private float posX, posY, vX, vY, rayon;
+	public float posX, posY, vX, vY, rayon, masse;
 	
-	public Balle(float posX, float posY, float vX, float vY, float rayon) {
+	public Balle(float posX, float posY, float vX, float vY, float rayon, float masse) {
 		this.posX = posX;
 		this.posY = posY;
 		this.vX = vX;
 		this.vY = vY;
 		this.rayon = rayon;
+		this.masse = masse;
 	}
 	
 	
-	public void majPosition(Espace spc, Boolean col) {
+	public void majPosition(Espace spc) {
 		
-		col = col != null ? col : false;
+		vX = (masse*spc.getVent() * spc.getTemps()) + vX;
+		vY = (masse*spc.getGravite() * spc.getTemps()) + vY;
+			
+		posY = posY + (vY * spc.getTemps());
+		posX = posX + (vX * spc.getTemps());
 		
-		vX = (spc.getVent() * spc.getTemps()) + vX;
-		vY = (spc.getGravite() * spc.getTemps()) + vY;
-		
-		if (col){
+		if ((posY - rayon) <= 0 || (posY + rayon) >= spc.getTailleY()) {
+			
+			if((posY - rayon) <=0)
+				posY = rayon; 
+			else
+				posY = spc.getTailleY() - rayon;
+			
 			vY = -vY;
+		}
+		if ((posX - rayon) <= 0.0 || (posX + rayon) >= spc.getTailleX()) {
+			if((posX - rayon) <= 0)
+				posX = rayon;
+			else
+				posX = spc.getTailleX() - rayon;	
 			vX = -vX;
-			
-			posY = posY + (vY * spc.getTemps());
-			posX = posX + (vX * spc.getTemps());
-		}else{
-			
-			posY = posY + (vY * spc.getTemps());
-			posX = posX + (vX * spc.getTemps());
-			
-			if ((posY - rayon) <= 0 || (posY + rayon) >= spc.getTailleY()) {
-				
-				if((posY - rayon) <=0)
-					posY = rayon; 
-				else
-					posY = spc.getTailleY() - rayon;
-				
-				vY = -vY;
-			}
-			if ((posX - rayon) <= 0.0 || (posX + rayon) >= spc.getTailleX()) {
-				if((posX - rayon) <= 0)
-					posX = rayon;
-				else
-					posX = spc.getTailleX() - rayon;	
-				vX = -vX;
-			}
 		}
 	} 
 
--- TP4/src/Main.java
+++ TP4/src/Main.java
@@ -1,12 +1,25 @@
 
 public class Main {
 	
-	public static Boolean Collision(Balle b, Balle b2){
+	public static void Collision(Balle b, Balle b2){
 		double d2 = (b.getPosX()-b2.getPosX())*(b.getPosX()-b2.getPosX()) + (b.getPosY()-b2.getPosY())*(b.getPosY()-b2.getPosY());
-					   if (d2 > (b.getRayon() + b2.getRayon())*(b.getRayon() + b2.getRayon()))
-					      return false;
-					   else
-					      return true;
+		   if (d2 > (b.getRayon() + b2.getRayon())*(b.getRayon() + b2.getRayon())){
+
+			   StdDraw.setPenColor(StdDraw.RED);
+			   StdDraw.filledCircle(b2.getPosX(), b2.getPosY(), b2.getRayon());
+/*			   float newVX = (b.vX * (b.masse - b2.masse) + (2 * b2.masse * b2.vX))  / (b.masse + b2.masse);
+			   float newVY = (b.vY * (b.masse - b2.masse) + (2 * b2.masse * b2.vY))  / (b.masse + b2.masse);
+			   float newVX2 = (b2.vX * (b2.masse - b.masse) + (2 * b.masse * b.vX))  / (b2.masse + b.masse);
+			   float newVY2 = (b2.vY * (b2.masse - b.masse) + (2 * b.masse * b.vY))  / (b2.masse + b.masse);
+			   b.vX = newVX;
+			   b.vY = newVY;
+			   b2.vX = newVX2;
+			   b2.vY = newVY2;   
+*/
+		   }else{
+			   StdDraw.setPenColor(StdDraw.GREEN);
+			   StdDraw.filledCircle(b2.getPosX(), b2.getPosY(), b2.getRayon());
+		   }
 	}
 
 	
@@ -14,8 +27,8 @@ public class Main {
 		 
 		 Espace terre = new Espace();
 		 
-		 Balle b = new Balle(15.0f, 45.0f, 0.0f,0.0f, 2.0f);
-		 Balle b2 = new Balle(20.0f, 40.0f, 0.0f,0.0f, 2.0f);
+		 Balle b = new Balle(15.0f, 45.0f, 0.0f,0.0f, 2.0f,1.0f);
+		 Balle b2 = new Balle(20.0f, 40.0f, 0.0f,0.0f, 3.0f,2.0f);
 		 StdDraw.setXscale(0.0,terre.getTailleX());
 		 StdDraw.setYscale(0.0,terre.getTailleY());
 		 
@@ -25,17 +38,12 @@ public class Main {
 			 StdDraw.setPenColor(StdDraw.BLUE);
 			 StdDraw.filledCircle(b.getPosX(), b.getPosY(), b.getRayon());
 			 
-			 StdDraw.setPenColor(StdDraw.RED);
-			 StdDraw.filledCircle(b2.getPosX(), b2.getPosY(), b2.getRayon());
+//			 StdDraw.setPenColor(StdDraw.RED);
+//			 StdDraw.filledCircle(b2.getPosX(), b2.getPosY(), b2.getRayon());
 			 
-			 Boolean col = Collision(b, b2);
-			 
-			 b.majPosition(terre,col);
-			 b2.majPosition(terre,col);
-			 
-			 if (Collision(b,b2)){
-				 
-			 }
+			 Collision(b, b2);
+			 b.majPosition(terre);
+			 b2.majPosition(terre);
 			 
 			 StdDraw.show(30);
 			 
